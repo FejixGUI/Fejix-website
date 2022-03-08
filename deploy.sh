e4700fe74ac45d353c9ea1ad804147e46fdcc2c5
MarkLagodych <lgd.mrk@gmail.com>
2022-03-09 00:38:18 +0200

    Generated website

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
 DIR=generated_website
 
 # Generate the site
-hugo -d $(DIR)
+hugo -d $DIR
 
-git add $(DIR)
+git add $DIR
 
 # Push the contents of DIR to the gh-pages branch
-git subtree push --prefix $(DIR) origin gh-pages
+git subtree push --prefix $DIR origin gh-pages
 
-git rm -rf $(DIR)
+git rm -rf $DIR
