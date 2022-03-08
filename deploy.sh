c3caa66807e899f2feb6da69efff343633f2b5c0
MarkLagodych <lgd.mrk@gmail.com>
2022-03-09 00:53:52 +0200

    Regenerated website after

--- deploy.sh
+++ deploy.sh
@@ -3,9 +3,5 @@ DIR=generated_website
 # Generate the site
 hugo -d $DIR
 
-git add $DIR
-
 # Push the contents of DIR to the gh-pages branch
 git subtree push --prefix $DIR origin gh-pages
-
-git rm -rf $DIR
